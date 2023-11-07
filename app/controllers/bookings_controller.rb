e1c59c43832f6030b612766d13f185caf7973afb
TF-PMcardle <pmcardle@.com>
2023-11-07 15:29:56 +0000

    [#7] Added delete function

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,14 +5,27 @@ class BookingsController < ApplicationController
     end
 
     def edit
-       
+        @booking = Booking.find(params[:id])
+        render :edit
+    end
+
+    def update
+        @booking = Passenger.find(params[:passenger_id])
+        if @booking.update(params.require(:passenger).permit(:Passenger.fname, :Passenger.sname, :Passenger.email))
+            flash[:success] = "Booking was updated successfully"
+            redirect_to bookings_path
+        else
+            flash.now[:error] = "Booking update failed"
+            render :edit
+        end
     end
 
     def destroy
-        @bookings = Booking.find(params[@bookings.id])
-        @bookings.destroy
-        redirect_to bookings_path
+        @booking = Booking.find(params[:id])
+        @booking.destroy
         flash[:success] = "Booking was deleted successfully"
+        redirect_to bookings_path
+        
     end
     
     def new
@@ -30,7 +43,7 @@ class BookingsController < ApplicationController
 
 private
     def booking_params
-        params.require(:booking).permit(:flight_id, :passenger_id)
+        params.require(:booking).permit(:id, :flight_id, :passenger_id)
     end
 end
 
